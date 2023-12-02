b496b29ea8ad42f09628176214464f060d2f72f8
hatakirri <iavlad228.55.0@gmail.com>
2023-12-02 17:26:55 +0300

    Add index & js

---
+++ script.js
@@ -0,0 +1,22 @@
+class Product{
+    imgLink;
+    productName;
+    productPrice;
+
+    constructor(imgLink, productName, productPrice){
+        this.imgLink = imgLink;
+        this.productName = productName;
+        this.productPrice = productPrice;
+        const container = document.createElement("div");
+        container.innerHTML = `
+        <div><img src = "${this.imgLink}">
+        <h3>Имя: ${this.productName}</h3>
+        <h5>Цена: ${this.productPrice}</h5>
+        <button> Купить </button>
+        </div>
+        `
+        document.body.append(container);
+    }
+}
+
+new Product("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSWfmRp-QOhqBek2fpsHeDvEMItE3WGT9Gibw&usqp=CAU.jpg", "Aizen", "infinity Bucks");
